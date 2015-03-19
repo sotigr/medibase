4939303ad1ec1b62909d44219cff8a6ad40bf19b
sotigr <sotiris.papaioannou@hotmail.com>
2015-03-19 21:09:20 +0200

    Style and database update

--- server/WebApp/WebApp/js/profilecreation.js
+++ server/WebApp/WebApp/js/profilecreation.js
@@ -16,3 +16,13 @@ $('#cp_submit').on('click', function () {
         alert("Warning!");
     }
 });
+$('#radio').buttonset();
+$('#radio').buttonset();
+$('#cp_title').selectmenu().selectmenu("menuWidget")
+.addClass("overflow");
+$('#day').selectmenu().selectmenu("menuWidget")
+.addClass("overflow");
+$('#month').selectmenu().selectmenu("menuWidget")
+.addClass("overflow");
+$('#year').selectmenu().selectmenu("menuWidget")
+.addClass("overflow");
